af2787a881ff76976f66c6b48746d0d57c321efb
Adriano Coutinho <adriano.coutinho@pagar.me>
2022-06-21 11:33:00 -0300

    chore: add security group for rds

--- kong-docker/security-groups.tf
+++ kong-docker/security-groups.tf
@@ -23,3 +23,32 @@ resource "aws_security_group" "kong" {
   tags = local.tags
 
 }
+
+resource "aws_security_group" "rds" {
+
+  name        = "${local.prefix}-rds-sg"
+  description = "Allow ${local.project} RDS Access"
+  vpc_id      = local.vpc_id
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = "Allow esgress to any ip"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = local.tags
+
+}
+
+resource "aws_security_group_rule" "rds_allow" {
+  security_group_id        = aws_security_group.kong.id
+  description              = "Permite acesso ao banco"
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.this.id
+}
